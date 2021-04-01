12b2abfd437810f6ad61efa6d2a7ca1e35215fc9
Matt92Martin <matt92martin@gmail.com>
2021-03-31 17:34:29 -0700

    Init

---
+++ index.js
@@ -0,0 +1,22 @@
+const path = require('path')
+const express = require('express')
+const cors = require('cors')
+
+const file1 = path.resolve(__dirname, 'files', 'file1.js')
+const file2 = path.resolve(__dirname, 'files', 'file2.js')
+
+const app = express()
+app.use(cors())
+
+app.get('*', (req, res) => {
+    if (Math.random() < 0.5){
+        res.sendFile(file1)
+    } else {
+        res.sendFile(file2)
+    }
+})
+
+
+app.listen(3000, () => {
+    console.log('Listening')
+})
